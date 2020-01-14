bb99c0a4b0b9c11d1eaff85b6e083a527b939d7e
xp660 <imxp660@gmail.com>
2020-01-14 23:35:27 +0800

    shortest_winForm

---
+++ find_Shortest/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace find_Shortest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBoxDistance = new System.Windows.Forms.TextBox();
+            this.textBoxRoute = new System.Windows.Forms.TextBox();
+            this.Texbox1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(69, 276);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.TextChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(278, 276);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 20);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            this.comboBox2.TextChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(451, 248);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBoxDistance
+            // 
+            this.textBoxDistance.Location = new System.Drawing.Point(161, 324);
+            this.textBoxDistance.Name = "textBoxDistance";
+            this.textBoxDistance.Size = new System.Drawing.Size(100, 22);
+            this.textBoxDistance.TabIndex = 3;
+            // 
+            // textBoxRoute
+            // 
+            this.textBoxRoute.Location = new System.Drawing.Point(161, 378);
+            this.textBoxRoute.Name = "textBoxRoute";
+            this.textBoxRoute.Size = new System.Drawing.Size(100, 22);
+            this.textBoxRoute.TabIndex = 4;
+            // 
+            // Texbox1
+            // 
+            this.Texbox1.AutoSize = true;
+            this.Texbox1.Location = new System.Drawing.Point(34, 279);
+            this.Texbox1.Name = "Texbox1";
+            this.Texbox1.Size = new System.Drawing.Size(29, 12);
+            this.Texbox1.TabIndex = 5;
+            this.Texbox1.Text = "起點";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(243, 279);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "終點";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(126, 327);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "距離";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(102, 381);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "經過節點";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 439);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Texbox1);
+            this.Controls.Add(this.textBoxRoute);
+            this.Controls.Add(this.textBoxDistance);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBoxDistance;
+        private System.Windows.Forms.TextBox textBoxRoute;
+        private System.Windows.Forms.Label Texbox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ find_Shortest/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace find_Shortest
+{
+    public partial class Form1 : Form
+    {
+        public const int N = 8; //N是總頂點數
+        const int NC = 100000;
+        //路網圖，　NC表示沒有直接連接
+        public static int[,] Cost = new int[,]  {{ 0,  3,  5,  4, NC, NC, NC, NC},
+                                                 { 3,  0, NC,  4, NC, NC, NC, NC},
+                                                 { 5, NC,  0,  3, NC,  2, NC, NC},
+                                                 { 4,  4,  3,  0,  7,  3, NC, NC},
+                                                 {NC, NC, NC,  7,  0, NC,  6,  1},
+                                                 {NC, NC,  2,  3, NC,  0,  4,  2},
+                                                 {NC, NC, NC, NC,  6,  4,  0,  3},
+                                                 {NC, NC, NC, NC,  1,  2,  3,  0}};
+        public static int[] Dist = new int[N];
+        public static int[] Decided = new int[N];
+        public static int[] PreNode = new int[N];
+        public static int Start, Destination;
+        string routeStr;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Start = 0; Destination = 0;
+            comboBox1.Text = "0"; comboBox2.Text = "0";
+            textBoxDistance.Text = "";
+            textBoxRoute.Text = "";
+            pictureBox1.Image = Image.FromFile("Route.jpg");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Start = Convert.ToInt16(comboBox1.Text);
+            Dijstra(Start);
+            textBoxDistance.Text = Dist[Destination].ToString();
+
+            routeStr = PrintPath(Start, Destination);
+            textBoxRoute.Text = routeStr;
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Destination = Convert.ToInt16(comboBox2.Text);
+            Dijstra(Start);
+            textBoxDistance.Text = Dist[Destination].ToString();
+            routeStr = PrintPath(Start, Destination);
+            textBoxRoute.Text = routeStr;
+        }
+
+        unsafe public static void Dijstra(int Start)
+        {
+            int Nd, i, w;
+            for (i = 0; i < N; i++)
+            {
+                Dist[i] = Cost[Start, i];   //初設為Cost[]陣列的第Start列
+                PreNode[i] = Start;          //由Start開始走到任意一點
+                Decided[i] = 0;
+            }
+
+            Decided[Start] = 1;
+
+            for (i = 0; i < N; i++)
+            {
+                find_Shortest(&Nd);
+                Decided[Nd] = 1;
+                for (w = 0; w < N; w++)
+                {
+                    if(Decided[w]==0 && Dist[w] > Dist[Nd] + Cost[Nd, w])
+                    {
+                        Dist[w] = Dist[Nd] + Cost[Nd, w];
+                        PreNode[w] = Nd;
+                    }
+                }
+            }
+        }
+
+        unsafe static void find_Shortest(int* Nd) //找出連接目前節點的所有點中距離最短的節點Nd
+        {
+            int low = 0, lowest = 32767, i = 0;
+            for (i=0; i < N; i++)
+            {
+                if(Decided[i]==0 && Dist[i] < lowest)
+                {
+                    lowest = Dist[i];
+                    low = i;
+                }
+            }
+            *Nd = low;
+        }
+
+        public static string PrintPath(int Start,int Nd) // Start: 起始點, Nd終點
+        {
+            int i;
+            Stack<int> stack = new Stack<int>();
+            string node;
+            stack.Push(Nd);
+            i = PreNode[Nd];
+            while (i != Start)
+            {
+                stack.Push(i);
+                i = PreNode[i];
+            }
+            stack.Push(i);
+            string routeStr = "";
+            while (stack.Count > 0)
+            {
+                node = stack.Pop().ToString();
+                routeStr = routeStr + " " + node;
+            }
+            return routeStr;
+        }
+
+    }
+}
