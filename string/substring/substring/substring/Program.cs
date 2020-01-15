94a678c46fb618cbb1fcc3554eb93610a7b39412
xp660 <imxp660@gmail.com>
2020-01-15 10:13:36 +0800

    substring

---
+++ string/substring/substring/substring/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] color = new string[2] { "Black", "White" };
+            foreach(string c in color)
+            {
+                Console.Write(c + " ");
+            }
+            Console.WriteLine();
+
+
+            //語法:string string.Substring(int StartIndex, int Length
+            string str = "Collections";
+            Console.WriteLine("string length:{0}", str.Length);
+            string substr = str.Substring(str.Length - 6, 6);
+            Console.WriteLine(substr);
+
+            Console.ReadKey();
+        }
+    }
+}
