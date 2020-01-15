8903574a07a10b265be07faa1ec529788d15a765
xp660 <imxp660@gmail.com>
2020-01-15 10:01:43 +0800

    empth

---
+++ string/empth/Empth/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empth
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            string name = "Chris Huang";
+            Console.WriteLine("Name = {0}", name);
+
+            name = String.Empty;
+
+            Console.WriteLine("Name = {0}", name);
+            Console.Read();
+        }
+    }
+}
