a7b76d9ad934043249d40510355c7403708a7f28
xp660 <imxp660@gmail.com>
2020-01-15 10:02:21 +0800

    length

---
+++ string/length/Length/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Length
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Chris Huang";
+            int stringLen = 0;
+
+            Console.WriteLine("String  = {0}", str);
+            
+            stringLen = str.Length;
+            Console.WriteLine("String Length = {0}", stringLen);
+
+            Console.WriteLine();
+            Console.WriteLine("Press Enter key to Exit!!");
+            Console.Read();
+        }
+    }
+}
---
+++ string/length/Split/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Split
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FileName = "tree.jpg";
+            string[] words = FileName.Split('.');
+            
+            foreach(string word in words)
+            {
+                Console.WriteLine(word);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
