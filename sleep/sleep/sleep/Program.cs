ecb9b855582789c9eb6de9eebe0a7ad0342284b7
xp660 <imxp660@gmail.com>
2020-01-27 10:03:17 +0800

    sleep

---
+++ sleep/sleep/sleep/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace sleep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.Sleep(1000); //暫停程式一秒
+        }
+    }
+}
