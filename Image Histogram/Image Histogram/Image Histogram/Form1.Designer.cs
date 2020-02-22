24093645ab1d1e39ec63ff6b4789107029229b5b
xp660 <imxp660@gmail.com>
2020-02-22 14:54:42 +0800

    Image Histogram

---
+++ Image Histogram/Image Histogram/Image Histogram/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Image_Histogram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.lblHistogram = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 284);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "load image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(384, 266);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(12, 313);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(384, 257);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3"});
+            this.comboBox1.Location = new System.Drawing.Point(707, 366);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.Text = "Y";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(472, 90);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(356, 259);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // lblHistogram
+            // 
+            this.lblHistogram.AutoSize = true;
+            this.lblHistogram.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblHistogram.Location = new System.Drawing.Point(471, 22);
+            this.lblHistogram.Name = "lblHistogram";
+            this.lblHistogram.Size = new System.Drawing.Size(109, 24);
+            this.lblHistogram.TabIndex = 5;
+            this.lblHistogram.Text = "histogram";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(901, 582);
+            this.Controls.Add(this.lblHistogram);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label lblHistogram;
+    }
+}
+
---
+++ Image Histogram/Image Histogram/Image Histogram/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+
+namespace Image_Histogram
+{
+    unsafe public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Paint += new PaintEventHandler(Form1_Paint);
+            
+            
+        }
+
+        Bitmap myBitmap = null;
+
+        private void button1_Click(object sender, EventArgs e) ////載入圖檔
+        {
+            this.openFileDialog1.Filter = "所有檔案|*.*|BMP File| *.bmp|JPEG File|*.jpg| GIF File|*.gif";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                myBitmap = new Bitmap(openFileDialog1.FileName);
+                pictureBox1.Image = myBitmap;
+                SwitchChannel(0);
+                comboBox1.Text = "Y";
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SwitchChannel(comboBox1.SelectedIndex);
+        }
+
+        public void SwitchChannel(int channel)
+        {
+            int[,,] ImgData = GetImgData(myBitmap);
+            long[] Values = new long[256];
+
+            ChannelProcess(ImgData, channel);
+            Bitmap processedBitmap = CreateBitmap(ImgData);
+            Values = GetHistogram(ImgData, channel);
+            DrawHistogram(Values);
+            lblHistogram.ForeColor = myColor;
+
+            pictureBox2.Image = processedBitmap;
+        }
+
+        private int[,,] GetImgData(Bitmap myBitmap)
+        {
+            int[,,] ImgData = new int[myBitmap.Width, myBitmap.Height, 3];
+            BitmapData byteArray = myBitmap.LockBits(new Rectangle(0, 0, myBitmap.Width, myBitmap.Height),
+                                                        ImageLockMode.ReadWrite,
+                                                        PixelFormat.Format24bppRgb);
+
+            int ByteOfSkip = byteArray.Stride - 3 * myBitmap.Width;
+            byte* imgPtr = (byte*)(byteArray.Scan0);
+
+            for (int y = 0; y < byteArray.Height; y++)
+            {
+                for (int x = 0; x < byteArray.Width; x++)
+                {
+                    ImgData[x, y, 2] = (int)*(imgPtr);      //B
+                    ImgData[x, y, 1] = (int)*(imgPtr + 1);  //G
+                    ImgData[x, y, 0] = (int)*(imgPtr + 2);  //R
+                    imgPtr += 3;
+
+                }
+
+                imgPtr += ByteOfSkip;
+            }
+
+            myBitmap.UnlockBits(byteArray);
+            return ImgData;
+
+        }
+
+        public static Bitmap CreateBitmap(int[,,] ImgData)
+        {
+            int Width = ImgData.GetLength(0);
+            int Height = ImgData.GetLength(1);
+            Bitmap myBitmap = new Bitmap(Width, Height, PixelFormat.Format24bppRgb);
+            BitmapData byteArray = myBitmap.LockBits(new Rectangle(0, 0, Width, Height),
+                                                            ImageLockMode.WriteOnly,
+                                                            PixelFormat.Format24bppRgb);
+
+            //Padding bytes的長度
+            int ByteOfSkip = byteArray.Stride - myBitmap.Width * 3;
+
+            byte* imgPtr = (byte*)byteArray.Scan0;
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    *imgPtr = (byte)ImgData[x, y, 2];       //B
+
+                    *(imgPtr + 1) = (byte)ImgData[x, y, 1];   //G
+
+                    *(imgPtr + 2) = (byte)ImgData[x, y, 0];   //R 
+                    
+                    imgPtr += 3;
+                }
+
+                imgPtr += ByteOfSkip; // 跳過Padding bytes
+            }
+
+            myBitmap.UnlockBits(byteArray);
+            return myBitmap;
+        }
+
+        long myMaxValue;
+        private long[] myValues;
+        private bool myIsDrawing;
+
+        private float myYUnit; //this gives the vertical unit used to scale our values
+        private float myXUnit; //this gives the horizontal unit used to scale our values
+
+        private int myOffset = 20; //the offset, in pixels, from the control margins.
+
+        private Color myColor = Color.Black;
+        private Font myFont = new Font("Tahoma", 10);
+
+        public long[] GetHistogram(int[,,] ImgData, int channel)
+        {
+            long[] myHistogram = new long[256];
+            int Width = ImgData.GetLength(0);
+            int Height = ImgData.GetLength(1);
+            for (int y = 0; y < Height; y++)
+                for (int x = 0; x < Width; x++)
+                {
+                    switch (channel)
+                    {
+
+                        case 0: //Gray
+                            int gray = ImgData[x, y, 1];
+                            myHistogram[gray]++;
+                            myColor = Color.Black;
+                            break;
+
+                        case 1:  //Red
+                            int Red = ImgData[x, y, 0];
+                            myHistogram[Red]++;
+                            myColor = Color.Red;
+                            break;
+                        case 2:  //Green
+                            int Green = ImgData[x, y, 1];
+                            myHistogram[Green]++;
+                            myColor = Color.Green;
+                            break;
+                        case 3:     //Blue
+                            int Blue = ImgData[x, y, 2];
+                            myHistogram[Blue]++;
+                            myColor = Color.Blue;
+                            break;
+                    }
+                }
+
+
+            return myHistogram;
+        }
+
+        private void ChannelProcess(int[,,] ImgData, int channel)
+        {
+            int Width = ImgData.GetLength(0);
+            int Height = ImgData.GetLength(1);
+
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    switch (channel)
+                    {
+
+                        case 0: //Gray
+                            int gray = (int)((double)ImgData[x, y, 0] * 0.299 + (double)ImgData[x, y, 1] * 0.587 + (double)ImgData[x, y, 2] * 0.114);
+                            ImgData[x, y, 0] = gray;
+                            ImgData[x, y, 1] = gray;
+                            ImgData[x, y, 2] = gray;
+                            break;
+
+                        case 1:  //Red
+                            ImgData[x, y, 1] = 0;
+                            ImgData[x, y, 2] = 0;
+                            break;
+                        case 2:  //Green
+                            ImgData[x, y, 0] = 0;
+                            ImgData[x, y, 2] = 0;
+                            break;
+                        case 3:     //Blue
+                            ImgData[x, y, 0] = 0;
+                            ImgData[x, y, 1] = 0;
+                            break;
+                    }
+                }
+            }
+        }
+        public void DrawHistogram(long[] Values)
+        {
+            myValues=new long[Values.Length];
+            Values.CopyTo(myValues, 0);
+            myIsDrawing = true;
+            myMaxValue = getMaxim(myValues);
+            
+            ComputeXYUnitValues();
+
+            //   this.pictureBox3.Invalidate();
+            this.Refresh();
+        }
+
+        private void ComputeXYUnitValues()
+        {
+            myYUnit = (float)(this.pictureBox3.Height - 2 * myOffset) / myMaxValue;
+            myXUnit = (float)(this.pictureBox3.Width - 2 * myOffset) / (myValues.Length - 1);
+        }
+
+        private long getMaxim(long[] Vals)
+        {
+            if (myIsDrawing)
+            {
+                long max = 0;
+                for (int i = 0; i < Vals.Length; i++)
+                {
+                    if (Vals[i] > max)
+                    {
+                        max = Vals[i];
+                    }
+                }
+                return max;
+            }
+
+            return 1;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (myIsDrawing)
+            {
+                Graphics g = this.pictureBox3.CreateGraphics();
+                Pen myPen = new Pen(myColor, 2);
+                int xPos, yPos;
+
+                for (int i = 0; i < myValues.Length; i++)
+                {
+                    xPos = myOffset + (int)(i * myXUnit);
+                    yPos = this.pictureBox3.Height - myOffset;
+                    g.DrawLine(myPen,
+                        new PointF(xPos, yPos),
+                        new PointF(xPos, yPos - (int)(myValues[i] * myYUnit)));
+                }
+                    g.DrawString("0", myFont, new SolidBrush(myColor), new PointF(myOffset, this.pictureBox3.Height - myFont.Height), System.Drawing.StringFormat.GenericDefault);
+                    g.DrawString((myValues.Length - 1).ToString(), myFont,new SolidBrush(myColor),
+                                    new PointF(myOffset + (myValues.Length * myXUnit) - g.MeasureString((myValues.Length - 1).ToString(), myFont).Width,
+                                                this.pictureBox3.Height - myFont.Height),
+                                   System.Drawing.StringFormat.GenericDefault);
+
+                    //Histogram周圍畫一個方型
+                    g.DrawRectangle(new System.Drawing.Pen(new SolidBrush(myColor), 2)
+                                    , myOffset, myOffset,
+                                    this.pictureBox3.Width - myOffset * 2, this.pictureBox3.Height - myOffset * 2);
+            }
+
+        }
+
+
+
+    }
+
+}
+
